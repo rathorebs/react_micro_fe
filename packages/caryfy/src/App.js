3fd5537c7dedd35deb3a37aa7fa27ea5597f3bfd
unknown <bhanwar>
2024-06-04 21:06:40 +0530

    ui fixes

--- packages/caryfy/src/App.js
+++ packages/caryfy/src/App.js
@@ -10,12 +10,12 @@ import Pricing from "./components/Pricing";
 import NewFile from "./components/NewFile.tsx";
 import ApolloClientProvider from "./providers/apollo-client";
 import OnBoardPayerInformation from "./pages/Management/OnBoardPayerInformation.tsx";
-import Agencyinformation from "./pages/Management/Agencyinformation.tsx"
+import Agencyinformation from "./pages/Management/Agencyinformation.tsx";
 import OnBoardPlanOfCare from "./pages/Management/OnBoardplanOfcare.tsx";
 import CaryfyDashBoard from "./pages/Dashboard/CaryfyDashBoard.tsx";
 import AskCaryPage from "./pages/AskCary/AskCaryPage.tsx";
 import CancelShift from "./pages/CancelShift/CancelShift.tsx";
-import Login from "./pages/Login.tsx"
+import Login from "./pages/Login.tsx";
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "ca",
@@ -28,7 +28,7 @@ export default ({ history }) => {
         <ApolloClientProvider>
           <Router history={history}>
             <Switch>
-              <Route path="/askcary" component={AskCaryPage} />
+              <Route path="caryfy/askcary" component={AskCaryPage} />
               <Route path="/cancelshift" component={CancelShift} />
               <Route path="/login" component={Login} />
               <Route path="/dash" component={CaryfyDashBoard} />
