598eaaa56ae4c1073a2e3acf6dbf63ee1bcddde3
unknown <bhanwar>
2024-06-05 18:35:35 +0530

    code refactoring

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,35 +1,34 @@
-import React from 'react';
-import { Switch, Route, Router } from 'react-router-dom';
+import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
-} from '@material-ui/core/styles';
+} from "@material-ui/core/styles";
 
-import Signin from './components/Signin';
-import Signup from './components/Signup';
-import NewFile from "./components/NewFile.tsx";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
 import ApolloClientProvider from "./providers/apollo-client";
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'au',
+  productionPrefix: "au",
 });
 
 export default ({ history, onSignIn }) => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
- <ApolloClientProvider>
-        <Router history={history}>
-          <Switch>
-            <Route path="/auth/signin">
-              <Signin onSignIn={onSignIn} />
-            </Route>
-            <Route path="/auth/signup">
-              <Signup onSignIn={onSignIn} />
-            </Route>
-          </Switch>
-        </Router>
-  </ApolloClientProvider>
+        <ApolloClientProvider>
+          <Router history={history}>
+            <Switch>
+              <Route path="/auth/signin">
+                <Signin onSignIn={onSignIn} />
+              </Route>
+              <Route path="/auth/signup">
+                <Signup onSignIn={onSignIn} />
+              </Route>
+            </Switch>
+          </Router>
+        </ApolloClientProvider>
       </StylesProvider>
     </div>
   );
