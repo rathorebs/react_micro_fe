a3d997dc35fd0964836988638e098ae8eb408545
unknown <bhanwar>
2024-06-12 09:53:26 +0530

    ui fixes

--- packages/caryfy/src/pages/NewTest/NewTestInner.tsx
+++ packages/caryfy/src/pages/NewTest/NewTestInner.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import NewTestInner from "./NewTestInner";
 export interface IAppProps {
 }
 
 export default function NewTest(props: IAppProps) {
   return (
     <div>
-      <p>Hello Home Page 9999{ }</p>
-      <NewTestInner />
+      Hello Home Page 9999 Inner BSR
     </div>
   );
 }
